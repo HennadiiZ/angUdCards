cf49c2bdc5ea93c1d978f63282946053870ef371
Hennadii <82006228+HennadiiZ@users.noreply.github.com>
2022-05-25 17:04:46 +0300

    refactoring hard code to more dynamic code / ngFor

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,6 +11,7 @@ export class AppComponent {
     {title: "Neat!!! Tree", url: "assets/Ukraine.png", userName:"Howdy", text: "123 Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque cupiditate corporis suscipit quam fuga iste, dolore soluta accusantium repellendus ullam?"},
     {title: "another title", url: "assets/Ukraine.png", userName:"Howker", text: "456 Lorem Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque cupiditate corporis suscipit quam fuga iste"},
     {title: "something else", url: "assets/Ukraine.png", userName:"Browdy", text: "789 Lorem Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque cupiditate corporis suscipit quam fuga iste, dolore soluta accusantium"},
+    {title: "something else", url: "assets/Ukraine.png", userName:"Browdy", text: "789 Lorem Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque cupiditate corporis suscipit quam fuga iste, dolore soluta accusantium"},
   ];
 
 
