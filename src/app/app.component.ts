43f01bf1f3cfbffb65b7679171bfade5da85e23a
Hennadii <82006228+HennadiiZ@users.noreply.github.com>
2022-05-25 17:22:57 +0300

    refactoring array of data to a method of data

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,12 +7,20 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
 
-  data = [
-    {title: "Neat!!! Tree", url: "assets/Ukraine.png", userName:"Howdy", text: "123 Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque cupiditate corporis suscipit quam fuga iste, dolore soluta accusantium repellendus ullam?"},
-    {title: "another title", url: "assets/Ukraine.png", userName:"Howker", text: "456 Lorem Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque cupiditate corporis suscipit quam fuga iste"},
-    {title: "something else", url: "assets/Ukraine.png", userName:"Browdy", text: "789 Lorem Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque cupiditate corporis suscipit quam fuga iste, dolore soluta accusantium"},
-    {title: "something else", url: "assets/Ukraine.png", userName:"Browdy", text: "789 Lorem Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque cupiditate corporis suscipit quam fuga iste, dolore soluta accusantium"},
-  ];
+  // data = [
+  //   {title: "Neat!!! Tree", url: "assets/Ukraine.png", userName:"Howdy", text: "123 Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque cupiditate corporis suscipit quam fuga iste, dolore soluta accusantium repellendus ullam?"},
+  //   {title: "another title", url: "assets/Ukraine.png", userName:"Howker", text: "456 Lorem Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque cupiditate corporis suscipit quam fuga iste"},
+  //   {title: "something else", url: "assets/Ukraine.png", userName:"Browdy", text: "789 Lorem Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque cupiditate corporis suscipit quam fuga iste, dolore soluta accusantium"},
+  //   {title: "something else", url: "assets/Ukraine.png", userName:"Browdy", text: "789 Lorem Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque cupiditate corporis suscipit quam fuga iste, dolore soluta accusantium"},
+  // ];
 
-
+  data(){ 
+  return [
+            {title: "Neat!!! Tree", url: "assets/Ukraine.png", userName:"Howdy", text: "123 Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque cupiditate corporis suscipit quam fuga iste, dolore soluta accusantium repellendus ullam?"},
+            {title: "another title", url: "assets/Ukraine.png", userName:"Howker", text: "456 Lorem Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque cupiditate corporis suscipit quam fuga iste"},
+            {title: "something else", url: "assets/Ukraine.png", userName:"Browdy", text: "789 Lorem Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque cupiditate corporis suscipit quam fuga iste, dolore soluta accusantium"},
+            {title: "something else", url: "assets/Ukraine.png", userName:"Browdy", text: "789 Lorem Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque cupiditate corporis suscipit quam fuga iste, dolore soluta accusantium"},
+            {title: "something else", url: "assets/Ukraine.png", userName:"Browdy", text: "789 Lorem Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque cupiditate corporis suscipit quam fuga iste, dolore soluta accusantium"},
+         ];
+  }
 }
