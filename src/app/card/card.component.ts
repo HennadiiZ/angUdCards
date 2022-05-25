06ee22b0cef05ce42a7317cb1cc9c0013136e92f
Hennadii <82006228+HennadiiZ@users.noreply.github.com>
2022-05-25 16:33:34 +0300

    Tying Data to a Component and Accepting Data in a Child Component

--- src/app/card/card.component.ts
+++ src/app/card/card.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-card',
@@ -7,9 +7,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CardComponent implements OnInit {
 
-  constructor() { }
+  @Input() title = "";
+  @Input() imageUrl = "";
+  @Input() text = "";
+  @Input() username = "";
 
-  ngOnInit(): void {
-  }
+  constructor() {}
+
+  ngOnInit(): void {}
 
 }
